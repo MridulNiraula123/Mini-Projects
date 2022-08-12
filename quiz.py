98a211080fff70df09beb33bf4c779b8799ee4d8
MridulNiraula123 <110655062+MridulNiraula123@users.noreply.github.com>
2022-08-12 10:01:51 +0545

    Create quiz.py

---
+++ quiz.py
@@ -0,0 +1,44 @@
+print("Welcome to my computer quiz!")
+score = 0
+
+playing = input("Do you want to play? ")
+
+if playing != "yes":
+    quit()
+
+print ("Okay! Let's Play :)")
+
+answer = input("what does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What is the name of the tallest mountain? ")
+if answer.lower() == "mount everest":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("Which game has reached 1 trillion views on Youtube? ")
+if answer.lower() == "minecraft":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("Which Country has more obese people than bycicles?   ")
+if answer.lower() == ("usa","united states of anerica"):
+    print('Correct!')
+    score+= 1
+else:
+    print("Incorrect!")
+
+print("You Got "+ str(score) + " questions correct!")
+print("You got" + str((score/4) * 100)+ " %")
+
+
+
+
