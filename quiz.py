5e4b77c133251409d162ff10e8496719e6bd4715
! Nirjal <81520082+Nirjal112233@users.noreply.github.com>
2022-08-12 10:11:24 +0545

    Update quiz.py
    Fixed all the bugs

--- quiz.py
+++ quiz.py
@@ -6,7 +6,7 @@ playing = input("Do you want to play? ")
 if playing != "yes":
     quit()
 
-print ("Okay! Let's Play :)")
+print("Okay! Let's Play :)")
 
 answer = input("what does CPU stand for? ")
 if answer.lower() == "central processing unit":
@@ -29,16 +29,12 @@ if answer.lower() == "minecraft":
 else:
     print("Incorrect!")
 
-answer = input("Which Country has more obese people than bycycles? ")
-if answer.lower() == ("usa"):
+answer = input("Which Country has more obese people than bicycles? ")
+if answer.lower() == "usa":
     print('Correct!')
-    score+= 1
+    score += 1
 else:
     print("Incorrect!")
 
-print("You Got "+ str(score) + " questions correct!")
-print("You got" + str((score/4) * 100)+ " %")
-
-
-
-
+print("You Got " + str(score) + " questions correct!")
+print("You got" + str((score / 4) * 100) + " %")
